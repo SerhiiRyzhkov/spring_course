39f672385df1d38d4e0c9fc442932ee8f3fcba6c
Serhii <had0uken@ukr.net>
2022-09-21 17:54:59 +0300

    21.09 commit

---
+++ src/spring_introduction/Test0.java
@@ -0,0 +1,13 @@
+package spring_introduction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static java.util.List.of;
+
+public class Test0 {
+    public static void main(String[] args) {
+
+    }
+}
